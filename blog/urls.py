62ae6079adce5bbd1f1f35b6ca5941412d855d87
Rahul Soni <stardustrahulsoni@gmail.com>
2021-11-03 20:10:46 +0530

    Added full repository for the daily dose blog website from local host to remote

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
-from .views import (PostListView,PostDetailView,PostCreateView,PostUpdateView,PostDeleteView,UserPostListView)
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView
+    )
 from . import views
 
 urlpatterns = [
@@ -7,7 +14,7 @@ urlpatterns = [
     path('post/<int:pk>/',PostDetailView.as_view(), name='post-detail'),
     path('post/<str:username>/',UserPostListView.as_view(), name='user-posts'),
     path('post/new/',PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/update',PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/update/',PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete',PostDeleteView.as_view(), name='post-delete'),
     path('about/',views.about, name='blog-about'),
 ]
